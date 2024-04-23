37ac0b96a4be93c1008111ed13ca697e8fcd3447
aldenluthfi <alden.luthfi@gmail.com>
2024-04-24 06:25:05 +0700

    Understanding subscriber and message broker.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+use crosstown_bus::{CrosstownBus, HandleError, MessageHandler};
 use std::{thread, time};
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+
 pub struct UserCreatedEventMessage {
     pub user_id: String,
-    pub user_name: String
+    pub user_name: String,
 }
+
 pub struct UserCreatedHandler;
-impl MessageHandler<UserCreatedEventMessage> for
-UserCreatedHandler {
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
     fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
         let ten_millis = time::Duration::from_millis(1000);
-        let now = time::Instant::now();
+        let _now = time::Instant::now();
 
         thread::sleep(ten_millis);
 
-        println!("In Upi's Computer [2206028932]. Message received: {:?}", message);
+        println!(
+            "In Upi's Computer [2206028932]. Message received: {:?}",
+            message
+        );
         Ok(())
     }
 
@@ -23,11 +28,19 @@ UserCreatedHandler {
         todo!()
     }
 }
-fn main() {
-    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
-    _ = listener.listen("user_created".to_owned(), UserCreatedHandler{}, crosstown_bus::QueueProperties { 
-        auto_delete: false, durable: false, use_dead_letter: true });
 
-    loop {
-    }
+fn main() {
+    let listener =
+        CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    _ = listener.listen(
+        "user_created".to_owned(),
+        UserCreatedHandler {},
+        crosstown_bus::QueueProperties {
+            auto_delete: false,
+            durable: false,
+            use_dead_letter: true,
+        },
+    );
+
+    loop {}
 }
